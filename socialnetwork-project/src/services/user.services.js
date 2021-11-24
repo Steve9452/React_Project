dfbc559998bef439a8865731d57516bcee4bcf7d
Steve Saravia <00380119@uca.edu.sv>
2021-11-23 22:03:15 -0600

    User context and services

---
+++ socialnetwork-project/src/services/user.services.js
@@ -0,0 +1,46 @@
+const BASE_URL = "https://posts-pw2021.herokuapp.com/api/v1";
+const services = [];
+
+
+services.login = async (username, password) => {
+
+    const response = await fetch(`${BASE_URL}/auth/signin`, 
+    {
+        headers: {
+            "Content-type" : "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    });
+
+    if(response.ok)
+    {
+        const data = await response.json();
+        return data;
+    }
+
+    return {};
+};
+
+services.verifyToken = async (token) => {
+
+    const response = await fetch(`${BASE_URL}/auth/whoami`,
+    {
+        headers: {
+            "Authorization" : `Bearer ${token}`
+        },
+        method: "GET"
+    });
+
+    if(!response) {
+        const data = await response.json();
+        return data;
+    }
+
+    return {};
+}
+
+export default services;
