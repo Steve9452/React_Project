47c4127bc7939fc7daada07523d046c5d1321651
bleczz <00400519@uca.edu.sv>
2021-11-28 11:33:52 -0600

    addding borders to posts

--- socialnetwork-project/src/pages/Admin/Post/Post.jsx
+++ socialnetwork-project/src/pages/Admin/Post/Post.jsx
@@ -1,24 +1,35 @@
-import { useState, useEffect } from "react";
-import { useUserContext } from "../../../context/UserContext";
+import { useState, useEffect } from 'react';
+import { useUserContext } from '../../../context/UserContext';
 import queryServices from '../../../services/query.services';
 import * as FontAwesome from 'react-icons/fa';
 
-import AdminOnly from "../AdminOnly";
-const Post = ({_id,userName,title,img,likes,description, comments, likeHandler, patchFav, toggletActive, active}) => {
-
+import AdminOnly from '../AdminOnly';
+const Post = ({
+  _id,
+  userName,
+  title,
+  img,
+  likes,
+  description,
+  comments,
+  likeHandler,
+  patchFav,
+  toggletActive,
+  active,
+}) => {
   // const {userName, title, img, likes, description} = post;
   const [toggleComments, settoggleComments] = useState(false);
-    
-    // const [likes, setLikes] = useState(0);
-    // const {userName, title, img, likes, description} = post; 
-    const notFoundImg = "https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png";
+
+  // const [likes, setLikes] = useState(0);
+  // const {userName, title, img, likes, description} = post;
+  const notFoundImg =
+    'https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png';
   return (
-    <li className="w-full p-4 hover:bg-lighter flex">
+    <li className="w-full p-4 hover:bg-lighter flex border-b-2">
       <section>
         <h2 className="font-semibold text-dark ml-2">{userName}</h2>
       </section>
 
-    
       <div>
         <h3 className="font-semibold text-dark ml-2">{title}</h3>
 
@@ -32,7 +43,12 @@ const Post = ({_id,userName,title,img,likes,description, comments, likeHandler, 
 
         <div className="flex items-center justify-between w-full">
           <section className="flex items-center text-sm text-dark space-x-20">
-            <button className="hover:bg-pink-800 hover:text-white rounded py-2 px-2" onClick={ () => {likeHandler(_id)}}>
+            <button
+              className="hover:bg-pink-800 hover:text-white rounded py-2 px-2"
+              onClick={() => {
+                likeHandler(_id);
+              }}
+            >
               Like
             </button>
 
@@ -40,7 +56,13 @@ const Post = ({_id,userName,title,img,likes,description, comments, likeHandler, 
               <FontAwesome.FaHeart />
               {likes.length}
             </p>
-            <button onClick = { () => {patchFav(_id)}}>Add Favorite</button>
+            <button
+              onClick={() => {
+                patchFav(_id);
+              }}
+            >
+              Add Favorite
+            </button>
             <div className="flex items-center justify-between">
               <FontAwesome.FaComment className="mr-5" />
               <button onClick={() => settoggleComments(!toggleComments)}>
@@ -48,11 +70,22 @@ const Post = ({_id,userName,title,img,likes,description, comments, likeHandler, 
               </button>
             </div>
 
-            {
-                toggleComments && (comments.map((com) => {return <li key={Date.now()}><p>{com.user.username}</p><p>{com.description}</p></li>}))                                
-            }
+            {toggleComments &&
+              comments.map((com) => {
+                return (
+                  <li key={Date.now()}>
+                    <p>{com.user.username}</p>
+                    <p>{com.description}</p>
+                  </li>
+                );
+              })}
 
-            <button onClick={() => toggletActive(_id)} className={active ? "":"bg-gray-300"}>Ocultar Post</button>
+            <button
+              onClick={() => toggletActive(_id)}
+              className={active ? '' : 'bg-gray-300'}
+            >
+              Ocultar Post
+            </button>
           </section>
         </div>
       </div>
